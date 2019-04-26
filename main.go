50403f05de3dc0d71460f3e77b7dd1ad70c2cbc4
Miavega <miguel_9504@hotmail.com>
2019-04-26 16:00:05 -0500

    First commit

---
+++ main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	_ "github.com/miavega/testApi/routers"
+
+	"github.com/astaxie/beego"
+	"github.com/astaxie/beego/orm"
+	_ "github.com/lib/pq"
+	"github.com/astaxie/beego/plugins/cors"
+	"github.com/astaxie/beego/logs"
+	"github.com/udistrital/utils_oas/customerror"
+)
+
+func main() {
+  orm.Debug = true
+  orm.RegisterDataBase("default", "postgres", beego.AppConfig.String("sqlconn"))
+  if beego.BConfig.RunMode == "dev" {
+  	beego.BConfig.WebConfig.DirectoryIndex = true
+  	beego.BConfig.WebConfig.StaticDir["/swagger"] = "swagger"
+  }
+  beego.InsertFilter("*", beego.BeforeRouter, cors.Allow(&cors.Options{
+    AllowOrigins: []string{"*"},
+    AllowMethods: []string{"PUT", "PATCH", "GET", "POST", "OPTIONS", "DELETE"},
+    AllowHeaders: []string{"Origin", "x-requested-with",
+      "content-type",
+      "accept",
+      "origin",
+      "authorization",
+      "x-csrftoken"},
+    ExposeHeaders:    []string{"Content-Length"},
+    AllowCredentials: true,
+  }))
+  logs.SetLogger(logs.AdapterFile, `{"filename":"/var/log/beego/pais_mid/pais_mid.log"}`)
+  beego.ErrorController(&customerror.CustomErrorController{})
+
+  beego.Run()
+}
+
